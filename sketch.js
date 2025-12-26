54d25afaabd2f723f93614cf7c6b60b95fa26e84
arjun-ku <arjun.94404@gmail.com>
2025-12-26 14:44:55 -0800

    Add files via upload
    if i type 1 it will do the same thing as piggo if i type 2 it will do the same thing as quack and if i type 3 it will do the same thing as human

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 volume = 1.5;
-let animal = prompt("what is your favorite animal?"); 
+let animal = prompt("what is your favorite animal? the options are piggo, quack and human if you tap on the animal it will make sound and if you type 1 it will do the same thing as piggo and if fyou type 2 it will do the same thing as quack and if you type 3 it will do the same thing as human"); 
 
 // 2. Check if the animal is "piggo"
-if (animal.toLowerCase() === "piggo") {
+if (Number(animal) === 1 || animal.toLowerCase() === "piggo") {
   alert("oink");
   // P5.js functions should be outside of the prompt logic
   function setup() {
@@ -36,7 +36,7 @@ function mousePressed(){
 }
 
 // 3. Check if the animal is "quack" (this is a separate check)
-if (animal.toLowerCase() === "quack") {
+if (Number(animal) === 2 || animal.toLowerCase() === "quack") {
   alert("quack");
   function setup() {
     createCanvas(400, 400);
@@ -72,7 +72,7 @@ function mousePressed(){
 //quack
 }
 
-if (animal.toLowerCase() === "human") {
+if (Number(animal) === 3 || animal.toLowerCase() === "human") {
   alert("hello i am winstin");
   // P5.js functions should be outside of the prompt logic
   function setup() {
@@ -117,4 +117,54 @@ function mousePressed(){
 }
 
 
+}
+// mouth
+ellipse(200, 240, 120, 136);
+//Ears
+triangle(50, 150, 100, 50, 150, 100);
+triangle(250, 100, 300, 50, 350, 150)
+
+
+function oink() {
+// Initialize the Howl object with your sound file(s)
+sound = new Howl({
+src: ['piggo.m4a'], // Howler supports multiple formats for compatibility: true
+});
+alert("piggo")
+}
+
+function mousePressed() {
+oink();
+sound.play();
+}
+
+// 3. Check if the animal is "quack" (this is a separate check)
+if (animal.toLowerCase() === "quack") {
+alert("quack");
+
+function setup() {
+createCanvas(400, 400);
+background("blue"); // You can change the background color if you like
+// Duck body (yellow ellipse)
+fill(255, 255, 0); // Yellow color in RGB
+ellipse(200, 250, 200, 150);
+// Duck head (yellow ellipse)
+ellipse(200, 150, 100, 100);
+// Beak (orange triangle)
+fill(255, 165, 0); // Orange color in RGB
+triangle(200, 150, 230, 140, 230, 160);
+// Eye (small white/black ellipse)
+fill(255); // White color
+ellipse(180, 140, 15, 15);
+fill(0); // Black color
+ellipse(180, 140, 5, 5);
+}
+}
+
+function quack() {
+// Initialize the Howl object with your sound file(s)
+sound = new Howl({
+src: ['quack.m4a'], // Howler supports multiple formats for compatibility: true
+});
+alert("quack")
 }
